0aff32f88ce467eb31a571754fa47fe0b50df486
MiguelcraftInGD <miguelcraft20101@gmail.com>
2026-05-12 17:33:05 -0300

    Update main.cpp AGAIN!

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,19 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/CreatorLayer.hpp>
-
 using namespace geode::prelude;
-
 class $modify(MyCreatorLayer, CreatorLayer) {
 	bool init() {
 		if (!CreatorLayer::init()) return false;
-
 		auto menu = this->getChildByID("creator-buttons-menu");
-
 		for (auto child : CCArrayExt<CCNode*>(menu->getChildren())) {
 			auto item = typeinfo_cast<CCMenuItemSpriteExtra*>(child);
 			if (!item) continue;
-
 			auto sprite = item->getNormalImage();
 			if (sprite) {
 				sprite->setScale(0.4f);
 			}
-
 			item->setContentSize({41.8f, 41.8f});
 		}
-
 		return true;
 	}
 };
